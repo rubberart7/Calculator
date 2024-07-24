c967a6b42e60af30d91be9795176815050dbd29f
Miraj Yafi <mbyafi7@outlook.com>
2024-07-23 22:47:35 -0400

    ability to add multiple digit numbers

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ function multiply(operand1, operand2) {
 }
 
 function divide(operand1, operand2) {
+    if (Number(operand2) === 0) {
+        alert("DIVIDE BY ZERO ERROR");
+        return;
+    }
     return Number(operand1) / Number(operand2);
 }
-
 
 function operate(operand1, operand2, operation) {
     let result;
@@ -31,11 +34,9 @@ function operate(operand1, operand2, operation) {
     return result;
 };
 
-
-
 const screen = document.querySelector(".screen");
 const buttonsArray = Array.from(document.querySelectorAll(".buttons button"));
-let numOperations = [];
+let numOperations = [""];
 // select all of the buttons and put those button elments into an array
 console.log(buttonsArray);
 
@@ -50,11 +51,17 @@ buttonsArray.forEach(button => {
         }
         else  {
             if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(value)) {
-                if (numOperations.length < 3) {
-                    screen.textContent = value;
+                if (numOperations.length < 2) {
+                    numOperations[0] += value;
+                    screen.textContent = numOperations[0];
+                    console.log(numOperations);
+                }
+
+                else if (numOperations.length === 2){
                     numOperations.push(value);
                     console.log(numOperations);
                 }
+
                 else {
                     console.log(numOperations);
                     let result = operate(numOperations[0], numOperations[2], numOperations[1]);
@@ -62,26 +69,44 @@ buttonsArray.forEach(button => {
                     screen.textContent = result;
                     numOperations.splice(0, 3);
                     numOperations.push(result);
+
                 }
 
             }
-            
+
+            // if you hit an operator again make sure to push operator, evaluate the stuff before and then push it the evaluation to the front
+
             else if (['+', '-', '*', '/'].includes(value)) {
                 numOperations.push(value);
                 console.log(numOperations);
             }
+            
+            // else if (['+', '-', '*', '/'].includes(value)) {
+            //     if (numOperations.length < 3) {
+            //         numOperations.push(value);
+            //         console.log(numOperations);
+            //     }
 
-            else if (value === "=") {
-                console.log(numOperations);
-                let result = operate(numOperations[0], numOperations[2], numOperations[1]);
-                console.log(`Result: ${result}`);
-                screen.textContent = result;
-                numOperations.splice(0, 3);
-                numOperations.push(result);
-                console.log(numOperations);
-            }
+            //     else {
+            //         console.log(numOperations);
+            //         let result = operate(numOperations[0], numOperations[2], numOperations[1]);
+            //         console.log(`Result: ${result}`);
+            //         screen.textContent = result;
+            //         numOperations.splice(0, 3);
+            //         numOperations.push(result);
 
-
+            //     }
+                
+            // }
+            // else if (value === "=") {
+            //     console.log(numOperations);
+            //     let result = operate(numOperations[0], numOperations[2], numOperations[1]);
+            //     console.log(`Result: ${result}`);
+            //     screen.textContent = result;
+            //     numOperations.splice(0, 3);
+            //     numOperations.push(result);
+            //     console.log(numOperations);
+            // }
         }
     });
 });
